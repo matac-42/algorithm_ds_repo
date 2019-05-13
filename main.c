50243f48b58eda412b1f1130fc89a8af31e086d0
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-05-14 01:15:23 +0900

    error fix

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-/*Program name: list.c */
 
 #include<stdio.h>
+#include <stdlib.h>
+
+int find_key();
+void delete_key();
 
 struct vertex{
     int name, key;
     struct vertex *next;
-}; *top;
+} *top;
+
 
 main(){
-    struct vertex *new, *top;
-    int n, k;
+    struct vertex *new;
+    int n, k, ans;
 
     printf("\n");
     printf("::::::::リストへのデータ入力::::::::: \n");
@@ -23,12 +27,21 @@ main(){
         if(n < 0) break;
         printf(" keyの値を入力してください：　");
         scanf("%d", &k);
-        new = (struct vertex *)malloc(sizeof(struct vertex));
-        new -> name = n;
-        new -> key = k;
-        new -> next = top;
-        top = new;
-
+        if(find_key(k) == 1){
+            printf("  key: %d は既登録です\n", k);
+            printf("    keyが %d のものを削除しますか？\n", k);
+            printf("      YES: 1 / No: 0 ==");
+            scanf("%d",& ans);
+            if(ans == 1) delete_key(k);
+            else printf("  もう一度 name の値から入力してください\\n");
+        }
+        else {
+            new = (struct vertex *) malloc(sizeof(struct vertex));
+            new->name = n;
+            new->key = k;
+            new->next = top;
+            top = new;
+        }
     }
 
     printf("\n");
@@ -42,18 +55,45 @@ main(){
     printf("\n");
 }
 
-find_key();
 
-find_key(int k)
+int find_key(int k)
 {
     struct vertex *w;
 
     w = top;
     while(w != NULL){
         if(k == w->key)
-            return(1);
+            return 1;
         w = w->next;
     }
     return 0;
 }
+
+void delete_key(int k)
+{
+    struct vertex *w, *prev, *tmp;
+    if(k == top->key){
+        tmp = top;
+        top = top->next;
+        free(tmp);
+    }
+    else{
+        prev = top;
+        w = prev->next;
+        while(w != NULL){
+            if(k ==  w->key){
+                prev->next = w->next;
+                free(w);
+                break;
+            }
+            prev = w;
+            w = w->next;
+        }
+    }
+}
+
+
+
+
+
 
